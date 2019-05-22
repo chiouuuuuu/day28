70af450c8b80a0055f39b957b0de7b135549acbc
chiouuuuuu <39206385+chiouuuuuu@users.noreply.github.com>
2019-05-22 21:23:12 +0800

    5.22.21.23

--- js/index.js
+++ js/index.js
@@ -1,44 +1,52 @@
-// 邮箱后缀List参考
-var postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
-window.onload=function () {  
-    //show();
-    var oin=document.getElementById("email-input");
-    var timer=null;
-    oin.onfocus=function(){
-        show();
-        oin.onkeyup=function(){
-            uodate(oin.value);
+ // 邮箱后缀List参考
+ var postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
+ window.onload = function () {
+     createLi();
+     var oin = document.getElementById("email-input");
+     oin.onfocus = function () {
+        if (oin.value != "") {
+            update(oin.value);
         }
-    }
+        oin.onkeyup = function () {
+            update(oin.value);
+        }
+     }
 
-    oin.onblur=function(){
-        oin.style.background="#fff";
-        killShow();
-    }
-    
-    function uodate(str){
-        var oul=document.getElementById("email-sug-wrapper");
-        var ali=oul.getElementsByTagName("li");
-        for(var i=0;i<ali.length;i++){
-            ali[i].innerHTML=str+"@"+postfixList[i];
-        }
-    }
+     oin.onblur = function () {
+        document.getElementById("email-sug-wrapper").style.display="none";
+     }
 
-    function show(){
-        killShow();
-        var oul=document.getElementById("email-sug-wrapper");
-        oul.className="gray-border";
-        for(var i=0;i<postfixList.length;i++){
-            var oli=document.createElement("li");
-            oli.innerHTML=postfixList[i];
-            oli.className="email-tag";
-            oul.appendChild(oli);
-        }
-    }
-    function killShow(){
-        clearInterval(timer);
-        var oul=document.getElementById("email-sug-wrapper");
-        oul.className="";
-        while(oul.children.length)oul.removeChild(oul.children[0]);
-    }
-}
+     function update(str) {
+         var oul = document.getElementById("email-sug-wrapper");
+         var ali = oul.getElementsByTagName("li");
+         for (var i = 0; i < ali.length; i++) {
+             ali[i].innerHTML = str + "@" + postfixList[i];
+             if(ali[i].scrollWidth>ali[i].clientWidth){
+                 alert("长度过长");
+             }
+         }
+         oul.style.display="block";
+     }
+
+     function createLi() {
+         var oul = document.getElementById("email-sug-wrapper");
+         oul.className = "gray-border";
+         for (var i = 0; i < postfixList.length; i++) {
+             var oli = document.createElement("li");
+             oli.innerHTML = postfixList[i];
+             oli.className = "email-tag";
+             oul.appendChild(oli);
+         }
+         oul.style.display="none";
+     }
+     document.getElementById("001").onclick=function () {
+        console.log(deleteSpace(oin.value));
+     }
+ }
+
+ function deleteSpace(str){
+    let l=0,r=str.length-1;
+    while(str[l]==' ')l++;
+    while(str[r]==' ')r--;
+    return str.substring(l,r+1);
+ }
