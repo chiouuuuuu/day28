5032786bdcf7d5f587f5124181e80719042d198f
chiouuuuuu <qaz951706893@163.com>
2019-05-23 23:39:45 +0800

    5.23.23.39

--- js/index.js
+++ js/index.js
@@ -6,42 +6,26 @@
      var oin = document.getElementById("email-input");
      oin.oninput = function () {
          var str = trim(oin.value);
-         str = preAt(str);
          update(str);
      }
 
      oin.onblur = function () {
          document.getElementById("email-sug-wrapper").style.display = "none";
      }
-
-    //  function update(str) {
-    //      if (str != "") {
-    //          var flag = true;
-    //          var ali = oul.getElementsByTagName("li");
-    //          for (var i = 0; i < ali.length; i++) {
-    //              ali[i].innerHTML = str + "@" + postfixList[i];
-    //              ali[i].flag=true;
-    //              /****判断用户名长度是否过长****/
-    //              if (ali[i].scrollWidth > ali[i].clientWidth) {
-    //                  oin.value = "";
-    //                  alert("长度过长");
-    //                  flag = false;
-    //              }
-    //          }
-    //          if (flag) oul.style.display = "block";
-    //          else oul.style.display = "none";
-    //      } else {
-    //          oul.style.display = "none";
-    //      }
-    //  }    
     
      function update(str) {
          if (str != "") {
-             var flag = true;
+             var flag = true;//if the flag is true the oul will display
+             var numMatched=0;
              var ali = oul.getElementsByTagName("li");
+             for(var i=0;i<ali.length;i++){
+                ali[i].style.display="block";
+             }
              for (var i = 0; i < ali.length; i++) {
-                 ali[i].innerHTML = str + "@" + postfixList[i];
-                 ali[i].flag=true;
+                 ali[i].innerHTML = preAt(str) + "@" + postfixList[i];
+                 ali[i].flag=isMatch(str,postfixList[i]);
+                 if(ali[i].flag)numMatched++;
+                 console.log(ali[i].flag);
                  /****判断用户名长度是否过长****/
                  if (ali[i].scrollWidth > ali[i].clientWidth) {
                      oin.value = "";
@@ -51,6 +35,11 @@
              }
              if (flag) oul.style.display = "block";
              else oul.style.display = "none";
+             for(var i=0;i<ali.length && numMatched>0;i++){
+                if(ali[i].flag==false){
+                    ali[i].style.display="none";
+                }
+             }
          } else {
              oul.style.display = "none";
          }
@@ -91,7 +80,7 @@
      }
  }
 
- function trim(str) {
+ function trim(str) {//delete the the space before and after the string that unnecessary
      let l = 0,
          r = str.length - 1;
      while (str[l] == ' ') l++;
@@ -102,7 +91,7 @@
          return "";
  }
 
- function isHaveAt(str) {//if string has a '@' return ture
+ function isHaveAt(str) {//if string has a '@' return the position of '@' else return -1
      var l = 0;
      var pos=-1;
      while (str[l] != "@" && l < str.length) l++;
@@ -110,7 +99,7 @@
      return pos;
  }
 
- function preAt(str) {
+ function preAt(str) {//return the string before '@'(if the string haven't '@' return the string)
      var l=isHaveAt(str);
      if (l==-1) return str;
      else return str.substring(0, l);
