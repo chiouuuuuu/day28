6e6cbbfe767cdb055b0bae407346de5e0cafe446
chiouuuuuu <qaz951706893@163.com>
2019-05-25 00:00:39 +0800

    5.25.0.00

--- js/index.js
+++ js/index.js
@@ -1,14 +1,55 @@
  // 邮箱后缀List参考
  var postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
+
+ var HtmlUnit={
+    Encode:function(str){
+        var tmp=document.createElement("div");
+        (tmp.innerText==undefined)?(tmp.innerText=str):(tmp.textContent=str);
+        var output=tmp.innerHTML;
+        tmp=null;
+        return output;
+    },
+    Decode:function(html) {
+        var tmp=document.createElement("div");
+        tmp.innerHTML=html;
+        var output=tmp.innerText||tmp.textContent;
+        tmp=null;
+        return output;
+    }
+}
+
  window.onload = function () {
      createLi();
      var oul = document.getElementById("email-sug-wrapper");
      var oin = document.getElementById("email-input");
+     var index=0;
+     var arr=[];
+
      oin.oninput = function () {
-         var str = trim(oin.value);
+         var str = HtmlUnit.Encode(trim(oin.value));
          update(str);
+         console.log(arr.length);
+     }
+     oin.onkeydown=function(event){
+        var e=event||window.event;
+        numMatched=arr.length;
+        if(e.keyCode==13){
+            console.log("key 13");
+        }
+        else if(e.keyCode==38 || e.keyCode==40){
+                e.preventDefault();
+                if(e.keyCode==38){
+                    index=(index+1)%numMatched;
+                    console.log("key 38");
+                }
+                if(e.keyCode==40){
+                    index=(index-1)%numMatched;
+                    console.log("key 40");
+                }
+        }
      }
 
+     //the chosen change color
      oul.onmouseover=function(event){
          var e=event || window.event;
          var target=e.target || e.srcElement;
@@ -17,6 +58,7 @@
          }
      }
     
+     //the chosen moveout
      oul.onmouseout=function(event){
         var e=event || window.event;
         var target=e.target || e.srcElement;
@@ -25,22 +67,23 @@
         }
     }
 
+    //click make the chosen show in input
     oul.onmousedown=function(event){
-        console.log("ok");
         var e=event || window.event;
         var target=e.target || e.srcElement;
         if(target.nodeName.toLowerCase()=="li"){
-            oin.value=target.innerHTML;
-            console.log(target.innerHTML);
+            oin.value=Html.Decode(target.innerHTML);
         }
         oul.style.display="none";
     }
 
+    //the focus move out
     oin.onblur=function(){
         oul.style.display="none";
     }
 
      function update(str) {
+         arr=[];
          if (str != "") {
              var flag = true;//if the flag is true the oul will display
              var numMatched=0;
@@ -66,6 +109,9 @@
                     ali[i].style.display="none";
                 }
              }
+             for(var i=0;i<ali.length;i++){
+                 if(ali[i].flag)arr.push(ali[i]);
+             }
          } else {
              oul.style.display = "none";
          }
@@ -82,6 +128,7 @@
          }
          oul.style.display = "none";
      }
+
      document.getElementById("001").onclick = function () {
          console.log(isMatch(oin.value,postfixList[0]));
      }
@@ -91,8 +138,8 @@
         var flag=true;
         if(l!=-1){
              var ss=str.substring(l+1,str.length);
-             console.log(ss);
-             console.log(arrElement);
+            //  console.log(ss);
+            //  console.log(arrElement);
              for(var i=0;i<ss.length;i++){
                  if(ss[i]!=arrElement[i]){
                     flag=false;
